11575d44e269dc98414d5f4717c09e474c3bd1fd
maidnl <d.aimo@arduino.cc>
2023-09-14 16:43:22 +0200

    Added boards capri and muxto

---
+++ hw/bsp/ra/boards/capri/board.mk
@@ -0,0 +1,14 @@
+CPU_CORE = cortex-m23
+MCU_VARIANT = ra2a1
+
+LD_FILE = ${BOARD_PATH}/${BOARD}.ld
+
+# For flash-jlink target
+JLINK_DEVICE = R7FA4M1AB
+
+USB_FIRST_BULK_PIPE=4
+USB_NO_ISOCRONOUS_PIPE=1
+USB_NUMBER_OF_PIPES=8
+
+
+flash: flash-jlink
---
+++ hw/bsp/ra/boards/muxto/board.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_SYSTEM_PROCESSOR cortex-m23 CACHE INTERNAL "System Processor")
+set(MCU_VARIANT ra42a1)
+
+set(JLINK_DEVICE R7FA2A1A)
+set(LD_FILE_GNU ${CMAKE_CURRENT_LIST_DIR}/${BOARD}.ld)
+
+function(update_board TARGET)
+#  target_compile_definitions(${TARGET} PUBLIC)
+#  target_sources(${TARGET} PRIVATE)
+#  target_include_directories(${BOARD_TARGET} PUBLIC)
+endfunction()
